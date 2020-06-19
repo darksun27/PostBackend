67bfaff65c624c52fd55d540c420ff7e3b6470d0
sunny dhama <blackwolf08>
2020-06-19 12:31:47 +0530

    added notification server and firebase

---
+++ expo/index.js
@@ -0,0 +1,68 @@
+const { Expo } = require("expo-server-sdk");
+let expo = new Expo();
+
+const sendNotification = async (data, tokens, body, title) => {
+  let messages = [];
+  for (let token of tokens) {
+    if (!Expo.isExpoPushToken(token)) {
+      console.error(`Push token ${token} is not a valid Expo push token`);
+      continue;
+    }
+    messages.push({
+      to: token,
+      sound: "default",
+      priority: "high",
+      vibrate: [0, 250, 250, 250],
+      body,
+      title,
+      data,
+    });
+  }
+
+  let chunks = expo.chunkPushNotifications(messages);
+  let tickets = [];
+
+  for (let chunk of chunks) {
+    try {
+      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+      console.log(ticketChunk);
+      tickets.push(...ticketChunk);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  let receiptIds = [];
+  for (let ticket of tickets) {
+    if (ticket.id) {
+      receiptIds.push(ticket.id);
+    }
+  }
+
+  let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+
+  for (let chunk of receiptIdChunks) {
+    try {
+      let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+      console.log(receipts);
+
+      for (let receiptId in receipts) {
+        let { status, message, details } = receipts[receiptId];
+        if (status === "ok") {
+          continue;
+        } else if (status === "error") {
+          console.error(
+            `There was an error sending a notification: ${message}`
+          );
+          if (details && details.error) {
+            console.error(`The error code is ${details.error}`);
+          }
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+module.exports = sendNotification;
--- index.js
+++ index.js
@@ -1,40 +1,47 @@
-require('dotenv').config();
-const express = require('express');
-const chalk = require('chalk');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const connectDB = require('./database/index.js');
+require("dotenv").config();
+const express = require("express");
+const chalk = require("chalk");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const connectDB = require("./database/index.js");
 const app = express();
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase/serviceAccount.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://jiitcompanion-new-4c6fb.firebaseio.com",
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer().array());
-app.use("/assets", express.static(__dirname+'/assets'));
+app.use("/assets", express.static(__dirname + "/assets"));
 
 connectDB.connect();
 
 //Importing Database Models
-require('./models/userModel');
-require('./models/commentModel');
-require('./models/postModel');
-require('./models/messMenuModel');
+require("./models/userModel");
+require("./models/commentModel");
+require("./models/postModel");
+require("./models/messMenuModel");
 
 //Importing Routes
-require('./routes/userRoutes')(app);
-require('./routes/postRoutes')(app);
-require('./routes/commentRoutes')(app);
-require('./routes/messRoutes')(app);
+require("./routes/userRoutes")(app);
+require("./routes/postRoutes")(app);
+require("./routes/commentRoutes")(app);
+require("./routes/messRoutes")(app);
 
-const server = app.listen(process.env.PORT || 3000, ()=> {
-    console.log("Server Started!");
-    console.log("Listening on PORT ", chalk.inverse.green(server.address().port));
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log("Server Started!");
+  console.log("Listening on PORT ", chalk.inverse.green(server.address().port));
 });
 
-process.on('SIGINT', ()=> {
-    console.log(chalk.red("\nServer Turn off Sequence Initiated!"));
-    console.log(chalk.yellow("Disconnecting from Database"));
-    connectDB.disconnect();
-    console.log(chalk.yellow("Closing Server!"));
-    server.close();
-    console.log(chalk.green("Turn Off Sequence Complete! Exiting"));
-    process.exit(1);
-})
+process.on("SIGINT", () => {
+  console.log(chalk.red("\nServer Turn off Sequence Initiated!"));
+  console.log(chalk.yellow("Disconnecting from Database"));
+  connectDB.disconnect();
+  console.log(chalk.yellow("Closing Server!"));
+  server.close();
+  console.log(chalk.green("Turn Off Sequence Complete! Exiting"));
+  process.exit(1);
+});
